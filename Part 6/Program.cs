ee7066fcf44c5fd70e73a035660f9c6b83aa7ba6
Ayeni <ayenchar248@GoTVDSB.ca>
2020-03-02 13:17:08 -0500

    Add project files.

---
+++ Part 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Part_6
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            int min;
+            int max;
+            int userNumber;
+
+
+            Console.WriteLine("Enter a minimum number: ");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a maximum number: ");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number between both: ");
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            while()
+           
+           
+        }
+    }
+}
